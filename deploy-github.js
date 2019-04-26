27ab9e92c8c6439cf64ccfa6c667561fee1fd701
Gyumeijie <18170879819@163.com>
2019-04-27 00:25:26 +0800

    add delopy in travis.yml

--- deploy-github.js
+++ deploy-github.js
@@ -5,7 +5,7 @@ ghpages.publish(
   'public',
   {
     branch: 'gh-pages',
-    repo: 'https://github.com/Gyumeijie/cheatsheets.io',
+    repo: 'https://github.com/Gyumeijie/cheatsheets.io'
   },
   () => {
     // eslint-disable-next-line no-console
